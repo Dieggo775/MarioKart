1730cd15d15e3ffbb4a675c2961b2b64e5b181d3
Dieggo775 <dieggo_taylor@hotmail.com>
2026-07-10 12:31:34 -0300

    Functions Chains

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const player1 = {
+    NOME : "Mario",
+    VELOCIDADE : 4,
+    MANOBRABILIDADE : 3,
+    PODER : 3,
+    PONTOS : 0,
+}
+
+const player2 = {
+    NOME : "Luigi",
+    VELOCIDADE : 3,
+    MANOBRABILIDADE : 4,
+    PODER : 4,
+    PONTOS : 0,
+}
+
+async function rollDice() {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+async function playRaceEngine(charactere1, character2) {
+
+}
+
+(async function main() {
+    console.log(`🚨 Corrida entre ${player1.NOME} e ${player2.NOME} começando...\n`);
+
+    await playRaceEngine(player1, player2);
+})(); //função auto-invocada
+
+// main();
